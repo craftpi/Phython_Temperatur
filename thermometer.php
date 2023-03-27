86248879da614d040874f3c38580aa0ab4f695a0
craftpi <schpius04@gmail.com>
2023-03-27 17:56:09 +0200

    Änderungen thermometer.php: Anpassung frame

--- thermometer.php
+++ thermometer.php
@@ -1,15 +1,15 @@
 +---------------------+
 |     Thermometer     |
 +---------------------+
-| -location: string    |
-| -unit: string        |
-| -temperature: float  |
-+---------------------+
-| +__init__(self, location: str, unit: str)|
-| +set_location(self, new_location: str) -> None|
-| +get_location(self) -> str|
-| +set_temp(self, temp: float) -> None|
+| -standort: string   |
+| -einheit: string    |
+| -temperatur: float  |
++-----------------------------------------------+
+| +__init__(self, standort: str, einheit: str)	|
+| +setstandort(self, new_standort: str) -> None	|
+| +getstandort(self) -> str						|
+| +settemperatur(self, temp: float) -> None		|
 | -convert_to_kelvin(self, temp: float) -> float|
-| +get_temp_c(self) -> float|
-| +get_temp_f(self) -> float|
-+---------------------+
+| +gettempc(self) -> float						|
+| +gettempf(self) -> float						|
++-----------------------------------------------+
